c7ba89f439ed712dd149271e3e71b8ae67d23339
Artem-Yampolskiy <Artem-Yampolskiy@users.noreply.github.com>
2021-06-06 01:20:45 +0300

    add massage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './sass/main.scss';
+import getRefs from './js/get-refs';
+import API from './js/fetchCountries';
 import countryCardTpl from './templates/country.hbs';
 import debounce from 'lodash.debounce';
-import { alert } from '@pnotify/core';
+import { error, notice } from '@pnotify/core';
 import '@pnotify/core/dist/BrightTheme.css';
-import API from './js/fetchCountries';
-import getRefs from './js/get-refs';
+import fetchCountries from './js/fetchCountries';
 
 const refs = getRefs();
 
@@ -12,40 +13,62 @@ refs.input.addEventListener('input', debounce(onInputChange, 500));
 
 function onInputChange(event) {
   const searchQuery = event.target.value;
+  clearMarkup();
+console.log(API.fetchCountries(searchQuery))
+  if (searchQuery.length < 1)
+    return;
 
-  API.fetchCountries(searchQuery).then(renderCountryMarkup).catch(onFetchError);
-}
+  API.fetchCountries(searchQuery)
+    .then(renderCountryMarkup)
+    .catch(onFetchError);
+};
 
 function renderCountryMarkup(countries) {
   clearMarkup();
+  if (countries.status === 404) {
+    pontyfyMassage('Nothing was found for your query!')
+  };
+
+  if (countries.length > 10) {
+    error({
+        text: 'Too many matches found. Please enter a more specific query!',
+        delay: 3000,
+        mouseReset: true,
+        sticker: false,
+    });
+  };
 
   if (countries.length === 1) {
     refs.list.insertAdjacentHTML('afterbegin', countryCardTpl(countries[0]));
-  }
+  };
 
-  if ((countries.length > 1) & (countries.length < 11)) {
+  if (countries.length > 1 && countries.length < 10) {
+    clearMarkup();
     createListCountriesMarkup(countries);
-  }
-
-  if (countries.length > 10) {
-    alert({
-      text: 'Too many matches found. Please enter a more specific query!',
-      delay: 2000,
-      mouseReset: true,
-      sticker: false,
-    });
-  }
-}
+  };  
+};
 
 function clearMarkup() {
   refs.list.innerHTML = '';
-}
+};
 
 function createListCountriesMarkup(countries) {
   const countriesList = countries.map(country => `<li>${country.name}</li>`).join('');
   return (refs.list.innerHTML = countriesList);
-}
+};
 
-function onFetchError(error) {
-  console.log(error);
+function onFetchError() {
+  refs.list.innerHTML = '';
+  notice({
+    title: 'OOPS!',
+    text: 'Invalid entered value. Try again =)',
+    delay: 2500,
+  });
+};
+
+function pontyfyMassage(message) {
+    error ({
+      title: `${message}`,
+      delay: 2000,
+    });
 }
